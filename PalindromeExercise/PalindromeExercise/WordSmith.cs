3192b2fc5f9fc3e6ab19331df34f17e377d08ba1
BenStrawbridge <lute_midnight@yahoo.com>
2020-11-16 14:36:41 -0600

    Palinedrome Unit Test Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            char[] chArr = word.ToCharArray();
+            Array.Reverse(chArr);
+
+            if (new string(chArr) == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("gary", false)]
+        public void PalindromeTest(string word, bool expected)
         {
+            // Arrange
+            var ws = new WordSmith();
 
+            // Act
+            var actual = ws.IsAPalindrome(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
